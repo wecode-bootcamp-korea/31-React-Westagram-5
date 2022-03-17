89232b0222e2691698f893190329d59342e236d7
Jaehyuksssss <ljh9510@naver.com>
2022-03-17 11:29:57 +0900

    modify rendering error

--- src/pages/jaehyuk/Login/LoginJaehyuk.js
+++ src/pages/jaehyuk/Login/LoginJaehyuk.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './LoginJaehyuk.scss';
 
 function Login() {
-  return <div> test </div>;
+  return (
+    <div className="wrap-1">
+      <div className="container-1">
+        <div className="main-box-1">
+          <div className="logo-box-1">
+            <p className="main-font">
+              <Link to="/Main">Westagram</Link>
+            </p>
+          </div>
+        </div>
+        <div className="input-form-1">
+          <input
+            id="id-input-1"
+            type="text"
+            placeholder="전화번호,사용자 이름 또는 이메일"
+          />
+          <input id="password-input-1" type="text" placeholder="비밀번호" />
+          <button id="login">
+            <span className="login-alphabet">로그인</span>
+          </button>
+        </div>
+        <div className="find-password-1">
+          <span className="find-password-2">비밀번호를 잊으셨나요?</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/jaehyuk/Main/MainJaehyuk.js
+++ src/pages/jaehyuk/Main/MainJaehyuk.js
@@ -1,7 +1,260 @@
 import React from 'react';
+import './MainJaehyuk.scss';
 
 function Main() {
-  return <div> test </div>;
+  return (
+    <div className="root-container">
+      <div className="nav">
+        <div className="insta-logo-box">
+          <img
+            id="instagram-icon"
+            src="/images/Jaehyuk/jh-images/icon-image/icon-insta.png"
+            alt=""
+          />
+
+          <div id="line"></div>
+          <p id="logo-name">Westagram</p>
+        </div>
+        <div className="search-box">
+          <input id="header-search-input" type="text" placeholder="검색" />
+        </div>
+        <div className="header-icon-box">
+          <img
+            alt=""
+            className="icon"
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+          />
+          <img
+            alt=""
+            className="icon"
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+          />
+          <img
+            alt=""
+            className="icon"
+            src={
+              'https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png'
+            }
+          />
+        </div>
+      </div>
+      <div className="main">
+        <div className="blank"></div>
+        <div className="feeds">
+          <div className="feeds-profile">
+            <div className="main-feeds">
+              <div className="main-profile-box">
+                <img
+                  alt=""
+                  className="profile-pic-1"
+                  src="/images/Jaehyuk/jh-images/profiles/main-profile.jpeg"
+                />
+                <p className="user-nickname">jaehyuksssss</p>
+              </div>
+              <img
+                alt=""
+                id="picture"
+                src="/images/Jaehyuk/jh-images/main-pic/feeds-pic.jpeg"
+              />
+
+              <div className="comment">
+                <div className="feeds-footer">
+                  <div className="comment-icon">
+                    <div className="comment-left-icon">
+                      <img
+                        alt=""
+                        className="icon"
+                        src="/images/Jaehyuk/jh-images/icon-image/heart.png"
+                      />
+                      <img
+                        alt=""
+                        className="icon"
+                        src="/images/Jaehyuk/jh-images/icon-image/comment.png"
+                      />
+                      <img
+                        alt=""
+                        className="icon"
+                        src="/images/Jaehyuk/jh-images/icon-image/direct-massage.png"
+                      />
+                    </div>
+                    <div className="comment-right-icon">
+                      <img
+                        alt=""
+                        className="icon"
+                        src="/images/Jaehyuk/jh-images/icon-image/mark.png"
+                      />
+                    </div>
+                  </div>
+                  <div className="comment">
+                    <div>
+                      <div className="likes-profile">
+                        <div className="comment-profile">
+                          <img
+                            alt=""
+                            className="comment-profile-pic"
+                            src="/images/Jaehyuk/jh-images/profiles/comment-pic.jpeg"
+                          />
+                        </div>
+                        <div className="likes">
+                          <span className="font">
+                            mong_dory님 외 100명이 좋아합니다
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="submit-form">
+                      <div id="input-comment">
+                        <div id="input-comment-view"></div>
+                        <div className="input-comment-text">
+                          <input
+                            id="input-text"
+                            name="댓글달기"
+                            placeholder="댓글달기.."
+                          />
+                          <button id="btn-submit">게시</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="main-right">
+          <div className="right-profile">
+            <div className="right-profile-id">
+              <div className="child-id">
+                <img
+                  alt=""
+                  className="right-profile-pic"
+                  src="/images/Jaehyuk/jh-images/profiles/child.jpg"
+                />
+                <span className="user-nickname">mong_dory</span>
+                <br />
+              </div>
+            </div>
+          </div>
+          <div className="story-box">
+            <div className="story-box-header">
+              <div className="story-add-box">
+                <div className="story-title">
+                  <span className="story-title">스토리</span>
+                </div>
+                <div className="see-more-button">
+                  <button className="add-more-button">모두 보기</button>
+                </div>
+              </div>
+              <div className="story-profile-box">
+                <div className="story-profile">
+                  <img
+                    alt=""
+                    className="story-profile-pic"
+                    src="/images/Jaehyuk/jh-images/profiles/story-pic-1.jpeg"
+                  />
+                  <p>
+                    <span className="user-nickname">Jaehyukss</span>
+                    <span className="recommand-user">회원님을 위한 추천</span>
+                  </p>
+                </div>
+                <div className="story-profile">
+                  <img
+                    alt=""
+                    className="story-profile-pic"
+                    src="/images/Jaehyuk/jh-images/profiles/story-pic-2.jpeg"
+                  />
+                  <p>
+                    <span className="user-nickname">yeony95</span>
+                    <span className="recommand-user">회원님을 위한 추천</span>
+                  </p>
+                </div>
+                <div className="story-profile">
+                  <img
+                    alt=""
+                    className="story-profile-pic"
+                    src="/images/Jaehyuk/jh-images/profiles/story-pic-3.jpeg"
+                  />
+                  <p>
+                    <span className="user-nickname">doraangs000</span>
+                    <span className="recommand-user">회원님을 위한 추천</span>
+                  </p>
+                </div>
+                <div className="story-profile">
+                  <img
+                    alt=""
+                    className="story-profile-pic"
+                    src="/images/Jaehyuk/jh-images/profiles/story-pic-4.jpeg"
+                  />
+                  <p>
+                    <span className="user-nickname">seeeni</span>
+                    <span className="recommand-user">회원님을 위한 추천</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="recommand-box">
+            <div className="recommand-box-header">
+              <div className="recommand-add-box">
+                <div className="recommand-title">
+                  <span className="recommand-title">회원님을 위한 추천</span>
+                </div>
+                <div className="see-more-button">
+                  <button className="add-more-button">모두 보기</button>
+                </div>
+              </div>
+              <div className="recommand-profile-box">
+                <div className="recommand-profile">
+                  <img
+                    alt=""
+                    className="recommand-profile-pic"
+                    src="/images/Jaehyuk/jh-images/profiles/recommand-1.jpeg"
+                  />
+                  <p>
+                    <span className="user-nickname">Jaehyuksssss</span>
+                    <span className="recommand-user">회원님을 위한 추천</span>
+                  </p>
+                </div>
+                <div className="recommand-profile">
+                  <img
+                    alt=""
+                    className="recommand-profile-pic"
+                    src="/images/Jaehyuk/jh-images/profiles/recommand-2.jpeg"
+                  />
+                  <p>
+                    <span className="user-nickname">yeony95</span>
+                    <span className="recommand-user">회원님을 위한 추천</span>
+                  </p>
+                </div>
+                <div className="recommand-profile">
+                  <img
+                    alt=""
+                    className="recommand-profile-pic"
+                    src="/images/Jaehyuk/jh-images/profiles/recommand-3.jpeg"
+                  />
+                  <p>
+                    <span className="user-nickname">doraangs000</span>
+                    <span className="recommand-user">회원님을 위한 추천</span>
+                  </p>
+                </div>
+                <div className="recommand-profile">
+                  <img
+                    alt=""
+                    className="story-profile-pic"
+                    src="/images/Jaehyuk/jh-images/profiles/recommand-4.jpeg"
+                  />
+                  <p>
+                    <span className="user-nickname">seeeni</span>
+                    <span className="recommand-user">회원님을 위한 추천</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
