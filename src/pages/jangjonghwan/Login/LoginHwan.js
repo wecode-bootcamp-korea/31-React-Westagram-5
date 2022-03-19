c3e9e9b78e52ae1dc4bb013713800930852535a2
장종환 <jjh970729@naver.com>
2022-03-19 14:30:17 +0900

    Add : Misson 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jangjonghwan/Login/LoginHwan.js
+++ src/pages/jangjonghwan/Login/LoginHwan.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginHwan.scss';
 
@@ -6,6 +6,16 @@ function Login() {
   const navigate = useNavigate();
   const goToMain = () => {
     navigate('/hwan/main');
+  };
+
+  const [userId, setUserId] = useState('');
+  const handleIdInput = e => {
+    setUserId(e.target.value);
+  };
+
+  const [userPassword, setUserPassword] = useState('');
+  const handlePwInput = e => {
+    setUserPassword(e.target.value);
   };
 
   return (
@@ -16,15 +26,20 @@ function Login() {
 
         <section className="loginButton">
           <input
-            className="loginId"
+            onChange={handleIdInput}
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
+            className="loginId"
+            value={userId}
           />
           <input
-            className="loginPassword"
+            onChange={handlePwInput}
             type="password"
             placeholder="비밀번호"
+            className="loginPassword"
+            value={userPassword}
           />
+
           <div className="pwButton">
             <button className="connect" type="button" onClick={goToMain}>
               로그인
