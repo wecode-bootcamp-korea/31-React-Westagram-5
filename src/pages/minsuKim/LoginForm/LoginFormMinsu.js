0e784b91dafa4d705fcfb9685b895200aa840edc
minsu <cocacolazero024@gmail.com>
2022-03-19 15:38:14 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/minsuKim/LoginForm/LoginFormMinsu.js
+++ src/pages/minsuKim/LoginForm/LoginFormMinsu.js
@@ -10,13 +10,35 @@ function LoginFormMinsu() {
   };
   const [idValue, setIdValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
+  const [btnColor, setBtnColor] = useState('black');
+  const [btndisabled, setBtnDisabled] = useState(true);
 
   const handleIdInput = event => {
     setIdValue({ id: event.target.value });
+
+    checkValidation();
   };
 
   const handlePasswordInput = event => {
     setPasswordValue({ password: event.target.value });
+
+    checkValidation();
+  };
+
+  const checkValidation = () => {
+    const emainCheck =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    const isValidId = emainCheck.test(idValue.id);
+    console.log('이메일 유효성 검사 :: ', isValidId);
+
+    const isValidPassword = passwordValue.password.length >= 8;
+    console.log(passwordValue.password.length, isValidPassword);
+
+    isValidId && isValidPassword
+      ? setBtnColor('#0095f6')
+      : setBtnColor('black');
+
+    isValidId && isValidPassword ? setBtnDisabled(false) : setBtnDisabled(true);
   };
 
   return (
@@ -25,22 +47,21 @@ function LoginFormMinsu() {
         <div>
           <input
             onChange={handleIdInput}
-            type="text"
-            required={true}
-            minLength={1}
             placeholder="Phone number, User name or Email"
           />
         </div>
         <div>
           <input
             onChange={handlePasswordInput}
-            required={true}
-            minLength={1}
             type="password"
             placeholder="Password"
           />
         </div>
-        <button type="submit" onClick={goToMain}>
+        <button
+          type="submit"
+          style={{ backgroundColor: btnColor }}
+          disabled={btndisabled}
+        >
           로그인
         </button>
       </form>
