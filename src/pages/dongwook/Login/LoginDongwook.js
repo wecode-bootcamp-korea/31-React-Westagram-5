0668a79f292bce6291febd5a5d6d9d3e0c656344
dan2dong <woozy345@gmail.com>
2022-03-20 21:05:39 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/dongwook/Login/LoginDongwook.js
+++ src/pages/dongwook/Login/LoginDongwook.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LoginDongwook.scss';
 import { Link } from 'react-router-dom';
 
-function handleIdInput(){
-
-}
-
 function LoginDongwook() {
+  const navigate = useNavigate();
+  const onClickImg = () => {
+    navigate('/dong/main');
+  };
+
+  const [userId, setUserId] = useState(' ');
+  const [userPassword, setUserPassword] = useState(' ');
+
+  const handleIdInput = e => {
+    setUserId(e.target.value);
+  };
+
+  const handlePwInput = e => {
+    setUserPassword(e.target.value);
+  };
+
   return (
     <div className="finalBox">
       <div className="fontBox">westagram</div>
-
 
       <div className="loginBox">
         <input
           type="text"
           className="idBox"
           placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={handleIdInput}
         />
-        <input type="password" className="passBox" placeholder="비밀번호" onChange={handleIdInput}/>
+        <input
+          type="password"
+          className="passBox"
+          placeholder="비밀번호"
+          onChange={handlePwInput}
+        />
         <Link to="/Main">
-          <button className="button">로그인</button>
+          <button className="button" onClick={onClickImg}>
+            로그인
+          </button>
         </Link>
       </div>
 
@@ -29,4 +49,3 @@ function LoginDongwook() {
   );
 }
 export default LoginDongwook;
-//export
