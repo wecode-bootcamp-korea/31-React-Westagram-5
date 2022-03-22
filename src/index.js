9e82866f201cd662a09205e663fbbdcd808a957a
minsu <cocacolazero024@gmail.com>
2022-03-22 18:42:51 +0900

    Modify : Refactoring

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Router from './Router';
 // fontawesome 추가
 import '@fortawesome/fontawesome-free/js/all.js';
-
+// scss추가
 import './styles/reset.scss';
 import './styles/common.scss';
 
--- src/pages/minsuKim/Main/MainMinsu.js
+++ src/pages/minsuKim/Main/MainMinsu.js
@@ -41,63 +41,85 @@ function MainMinsu() {
   return (
     <>
       <Nav />
-      <body className="Main">
-        <main className="mainContainer">
-          <article>
-            <section className="feed">
-              <div className="feedHeader">
-                <div>
-                  <img
-                    alt="profile_image"
-                    src="/images/minsuKim/baby-shiba-inu.jpeg"
-                  />
-                  <div className="feedHeaderName">
-                    <p>Shiba_Inu</p>
-                    <span>반포한강공원</span>
-                  </div>
-                </div>
-                <i className="fa-solid fa-ellipsis"></i>
-              </div>
-              <div className="feedImage">
-                <img alt="feedImage" src="/images/minsuKim/han-river.jpg" />
-              </div>
-              <div className="commentWrapper">
-                <div className="commentIcon">
-                  <div>
-                    <i className="fa-solid fa-heart"></i>
-                    <i className="fa-solid fa-comment"></i>
-                    <i className="fa-solid fa-envelope-open"></i>
-                  </div>
-                  <i className="fa-solid fa-bookmark"></i>
-                </div>
-
-                <div className="commentLike">
-                  <i className="fa-solid fa-circle-user"></i>
-                  <p>
-                    <span className="userName">Runner.jay</span>님 외 20명이
-                    좋아합니다.
-                  </p>
-                </div>
-                <CommentMinsu commentInputArray={commentInputArray} />
-              </div>
-
-              <form className="comment" onSubmit={submitComment}>
-                <input
-                  id="commentInput"
-                  onChange={addInput}
-                  type="text"
-                  value={commentInput}
-                  placeholder="댓글달기..."
+      <main className="mainContainer">
+        <article>
+          <section className="feed">
+            <div className="feedHeader">
+              <div>
+                <img
+                  alt="profile_image"
+                  src="/images/minsuKim/baby-shiba-inu.jpeg"
                 />
-                <button id="submit">게시</button>
-              </form>
-            </section>
-          </article>
+                <div className="feedHeaderName">
+                  <p>Shiba_Inu</p>
+                  <span>반포한강공원</span>
+                </div>
+              </div>
+              <i className="fa-solid fa-ellipsis"></i>
+            </div>
+            <div className="feedImage">
+              <img src="/images/minsuKim/han-river.jpg" />
+            </div>
+            <div className="commentWrapper">
+              <div className="commentIcon">
+                <div>
+                  <i className="fa-solid fa-heart"></i>
+                  <i className="fa-solid fa-comment"></i>
+                  <i className="fa-solid fa-envelope-open"></i>
+                </div>
+                <i className="fa-solid fa-bookmark"></i>
+              </div>
 
-          <aside>
+              <div className="commentLikeCount">
+                <span>좋아요 25개</span>
+              </div>
+
+              <div className="commentLike">
+                <i className="fa-solid fa-circle-user"></i>
+                <p>
+                  <span className="userName">Runner.jay</span>님 외 20명이
+                  좋아합니다.
+                </p>
+              </div>
+              <CommentMinsu commentInputArray={commentInputArray} />
+            </div>
+
+            <form className="comment" onSubmit={submitComment}>
+              <input
+                id="commentInput"
+                onChange={addInput}
+                type="text"
+                value={commentInput}
+                placeholder="댓글달기..."
+              />
+              <button id="submit">게시</button>
+            </form>
+          </section>
+        </article>
+
+        <aside>
+          <section className="sectionWrapper">
+            <div className="circledImage">
+              <img alt="profile-pic" src="/images/minsuKim/shiba.png" />
+              <img
+                className="liveCircle"
+                alt="story-circle"
+                src="/images/minsuKim/live-circle.png"
+              />
+            </div>
+            <div className="sectionInner">
+              <p>lifelog_msms</p>
+              <p>하루하루 최선을...</p>
+            </div>
+          </section>
+          <div className="contentWrapper">
+            <div className="contentHeader">
+              <p>스토리</p>
+              <p>모두 보기</p>
+            </div>
             <section className="sectionWrapper">
               <div className="circledImage">
-                <img alt="profile-pic" src="/images/minsuKim/shiba.png" />
+                <img alt="profile-pic" src="/images/minsuKim/jjangu.jpg" />
                 <img
                   className="liveCircle"
                   alt="story-circle"
@@ -105,112 +127,92 @@ function MainMinsu() {
                 />
               </div>
               <div className="sectionInner">
-                <p>Minsu_Kim</p>
-                <p>하루하루 최선을...</p>
+                <p>짱구</p>
+                <p>짱구는 못말려</p>
               </div>
             </section>
-            <div className="contentWrapper">
-              <div className="contentHeader">
-                <p>스토리</p>
-                <p>모두 보기</p>
+            <section className="sectionWrapper">
+              <div className="circledImage">
+                <img alt="profile-pic" src="/images/minsuKim/chulsoo.jpeg" />
+                <img
+                  className="liveCircle"
+                  alt="story-circle"
+                  src="/images/minsuKim/live-circle.png"
+                />
               </div>
-              <section className="sectionWrapper">
-                <div className="circledImage">
-                  <img alt="profile-pic" src="/images/minsuKim/jjangu.jpg" />
-                  <img
-                    className="liveCircle"
-                    alt="story-circle"
-                    src="/images/minsuKim/live-circle.png"
-                  />
-                </div>
-                <div className="sectionInner">
-                  <p>짱구</p>
-                  <p>짱구는 못말려</p>
-                </div>
-              </section>
-              <section className="sectionWrapper">
-                <div className="circledImage">
-                  <img alt="profile-pic" src="/images/minsuKim/chulsoo.jpeg" />
-                  <img
-                    className="liveCircle"
-                    alt="story-circle"
-                    src="/images/minsuKim/live-circle.png"
-                  />
-                </div>
-                <div className="sectionInner">
-                  <p>철수</p>
-                  <p>학원가기 싫다</p>
-                </div>
-              </section>
-              <section className="sectionWrapper">
-                <div className="circledImage">
-                  <img alt="profile-pic" src="/images/minsuKim/hoon.jpeg" />
-                  <img
-                    className="liveCircle"
-                    alt="story-circle"
-                    src="/images/minsuKim/live-circle.png"
-                  />
-                </div>
+              <div className="sectionInner">
+                <p>철수</p>
+                <p>학원가기 싫다</p>
+              </div>
+            </section>
+            <section className="sectionWrapper">
+              <div className="circledImage">
+                <img alt="profile-pic" src="/images/minsuKim/hoon.jpeg" />
+                <img
+                  className="liveCircle"
+                  alt="story-circle"
+                  src="/images/minsuKim/live-circle.png"
+                />
+              </div>
 
-                <div className="sectionInner">
-                  <p>훈이</p>
-                  <p>나는 주먹밥머리</p>
-                </div>
-              </section>
-              <section className="sectionWrapper">
-                <div className="circledImage">
-                  <img alt="profile-pic" src="/images/minsuKim/maenggu.png" />
-                  <img
-                    className="liveCircle"
-                    alt="story-circle"
-                    src="/images/minsuKim/live-circle.png"
-                  />
-                </div>
-                <div className="sectionInner">
-                  <p>맹구</p>
-                  <p>나는 돌이 좋아</p>
-                </div>
-              </section>
-              <section className="sectionWrapper">
-                <div className="circledImage">
-                  <img alt="profile-pic" src="/images/minsuKim/yuri.jpeg" />
-                  <img
-                    className="liveCircle"
-                    alt="story-circle"
-                    src="/images/minsuKim/live-circle.png"
-                  />
-                </div>
-                <div className="sectionInner">
-                  <p>유리</p>
-                  <p>나는 토끼인형이 좋아</p>
-                </div>
-              </section>
-              <section className="sectionWrapper">
-                <div className="circledImage">
-                  <img alt="profile-pic" src="/images/minsuKim/wonjang.jpeg" />
-                  <img
-                    className="liveCircle"
-                    alt="story-circle"
-                    src="/images/minsuKim/live-circle.png"
-                  />
-                </div>
-                <div className="sectionInner">
-                  <p>원장쌤</p>
-                  <p>떡잎마을 유치원 화이팅</p>
-                </div>
-              </section>
-            </div>
-            <section className="recommendationWrapper"></section>
-          </aside>
-        </main>
+              <div className="sectionInner">
+                <p>훈이</p>
+                <p>나는 주먹밥머리</p>
+              </div>
+            </section>
+            <section className="sectionWrapper">
+              <div className="circledImage">
+                <img alt="profile-pic" src="/images/minsuKim/maenggu.png" />
+                <img
+                  className="liveCircle"
+                  alt="story-circle"
+                  src="/images/minsuKim/live-circle.png"
+                />
+              </div>
+              <div className="sectionInner">
+                <p>맹구</p>
+                <p>나는 돌이 좋아</p>
+              </div>
+            </section>
+            <section className="sectionWrapper">
+              <div className="circledImage">
+                <img alt="profile-pic" src="/images/minsuKim/yuri.jpeg" />
+                <img
+                  className="liveCircle"
+                  alt="story-circle"
+                  src="/images/minsuKim/live-circle.png"
+                />
+              </div>
+              <div className="sectionInner">
+                <p>유리</p>
+                <p>나는 토끼인형이 좋아</p>
+              </div>
+            </section>
+            <section className="sectionWrapper">
+              <div className="circledImage">
+                <img alt="profile-pic" src="/images/minsuKim/wonjang.jpeg" />
+                <img
+                  className="liveCircle"
+                  alt="story-circle"
+                  src="/images/minsuKim/live-circle.png"
+                />
+              </div>
+              <div className="sectionInner">
+                <p>원장쌤</p>
+                <p>떡잎마을 유치원 화이팅</p>
+              </div>
+            </section>
+          </div>
+          <section className="recommendationWrapper"></section>
+        </aside>
+      </main>
 
-        <script
-          src="https://kit.fontawesome.com/d33ee304fc.js"
-          crossorigin="anonymous"
-        ></script>
+      <script
+        src="https://kit.fontawesome.com/d33ee304fc.js"
+        crossorigin="anonymous"
+      ></script>
 
-        <script src="js/main.js"></script>
-      </body>
+      <script src="js/main.js"></script>
     </>
   );
 }
