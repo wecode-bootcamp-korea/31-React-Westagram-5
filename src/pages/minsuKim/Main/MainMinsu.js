fee88bd044442ccac55b44e42a432e603b56555a
minsu <cocacolazero024@gmail.com>
2022-03-23 18:26:00 +0900

    ADD : Feed Componenet화

--- src/pages/minsuKim/Main/MainMinsu.js
+++ src/pages/minsuKim/Main/MainMinsu.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import Nav from '../../../components/Nav/Nav';
 import CommentMinsu from '../comment/CommentMinsu';
+import FeedMinsu from '../feed/FeedMinsu';
 import './MainMinsu.scss';
 
 function MainMinsu() {
@@ -46,61 +47,12 @@ function MainMinsu() {
     <>
       <Nav />
       <main className="mainContainer">
-        <article>
-          <section className="feed">
-            <div className="feedHeader">
-              <div>
-                <img
-                  alt="profile_image"
-                  src="/images/minsuKim/baby-shiba-inu.jpeg"
-                />
-                <div className="feedHeaderName">
-                  <p>Shiba_Inu</p>
-                  <span>반포한강공원</span>
-                </div>
-              </div>
-              <i className="fa-solid fa-ellipsis"></i>
-            </div>
-            <div className="feedImage">
-              <img src="/images/minsuKim/han-river.jpg" />
-            </div>
-            <div className="commentWrapper">
-              <div className="commentIcon">
-                <div>
-                  <i className="fa-solid fa-heart"></i>
-                  <i className="fa-solid fa-comment"></i>
-                  <i className="fa-solid fa-envelope-open"></i>
-                </div>
-                <i className="fa-solid fa-bookmark"></i>
-              </div>
-
-              <div className="commentLikeCount">
-                <span>좋아요 25개</span>
-              </div>
-
-              <div className="commentLike">
-                <i className="fa-solid fa-circle-user fa-lg"></i>
-                <p>
-                  <span className="userName">jjangu.luv</span>님 외 20명이
-                  좋아합니다.
-                </p>
-              </div>
-              <CommentMinsu commentInputArray={commentInputArray} />
-            </div>
-
-            <form className="comment" onSubmit={submitComment}>
-              <input
-                id="commentInput"
-                onChange={addInput}
-                type="text"
-                // value={commentInput}
-                placeholder="댓글달기..."
-                ref={commentInputRef}
-              />
-              <button id="submit">게시</button>
-            </form>
-          </section>
-        </article>
+        <FeedMinsu
+          commentInputArray={commentInputArray}
+          submitComment={submitComment}
+          addInput={addInput}
+          commentInputRef={commentInputRef}
+        />
 
         <aside>
           <section className="sectionWrapper">
---
+++ src/pages/minsuKim/feed/FeedMinsu.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import CommentMinsu from '../comment/CommentMinsu';
+
+const FeedMinsu = props => {
+  return (
+    <article>
+      <section className="feed">
+        <div className="feedHeader">
+          <div>
+            <img
+              alt="profile_image"
+              src="/images/minsuKim/baby-shiba-inu.jpeg"
+            />
+            <div className="feedHeaderName">
+              <p>Shiba_Inu</p>
+              <span>반포한강공원</span>
+            </div>
+          </div>
+          <i className="fa-solid fa-ellipsis"></i>
+        </div>
+        <div className="feedImage">
+          <img src="/images/minsuKim/han-river.jpg" />
+        </div>
+        <div className="commentWrapper">
+          <div className="commentIcon">
+            <div>
+              <i className="fa-solid fa-heart"></i>
+              <i className="fa-solid fa-comment"></i>
+              <i className="fa-solid fa-envelope-open"></i>
+            </div>
+            <i className="fa-solid fa-bookmark"></i>
+          </div>
+
+          <div className="commentLikeCount">
+            <span>좋아요 25개</span>
+          </div>
+
+          <div className="commentLike">
+            <i className="fa-solid fa-circle-user fa-lg"></i>
+            <p>
+              <span className="userName">jjangu.luv</span>님 외 20명이
+              좋아합니다.
+            </p>
+          </div>
+          <CommentMinsu commentInputArray={props.commentInputArray} />
+        </div>
+
+        <form className="comment" onSubmit={props.submitComment}>
+          <input
+            id="commentInput"
+            onChange={props.addInput}
+            type="text"
+            // value={commentInput}
+            placeholder="댓글달기..."
+            ref={props.commentInputRef}
+          />
+          <button id="submit">게시</button>
+        </form>
+      </section>
+    </article>
+  );
+};
+
+export default FeedMinsu;
