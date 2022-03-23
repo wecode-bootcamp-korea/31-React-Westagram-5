d9c21e89eccb182c0c4fb88fb99168b944a0297a
Kyungsuh Park <pyo05360@gmail.com>
2022-03-23 22:17:35 +0900

    Add: aside mock data로 컴포넌트화

--- src/pages/kyungsuh/Main/Recommend/Recommend.js
+++ src/pages/kyungsuh/Main/Recommend/Recommend.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import RecommendItem from '../RecommendItem/RecommendItem';
+import React, { useState, useEffect } from 'react';
 
 function Recommend() {
+  const [Recommend, setRecommend] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/kyungsuh/asideData.json')
+      .then(res => res.json())
+      .then(data => setRecommend(data));
+  }, []);
+
   return (
     <div className="recomBox">
       <div className="header">
@@ -9,12 +16,20 @@ function Recommend() {
         <button className="btn">모두 보기</button>
       </div>
       <div className="storyList">
-        <RecommendItem />
-        <RecommendItem />
-        <RecommendItem />
-        <RecommendItem />
-        <RecommendItem />
-        <RecommendItem />
+        {Recommend.map(list => {
+          return (
+            <div className="storyItem" key={list.id}>
+              <div>
+                <img src="/images/kyungsuh/basic-profile-img.png" alt="" />
+              </div>
+              <div className="right">
+                <span>{list.name}</span>
+                <span>{list.time}</span>
+              </div>
+              <button className="follow">팔로우</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/kyungsuh/Main/StoryForm/StoryForm.js
+++ src/pages/kyungsuh/Main/StoryForm/StoryForm.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import Storyitem from '../Storyitem/Storyitem';
+import React, { useState, useEffect } from 'react';
 
 function StoryForm() {
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/kyungsuh/asideData.json')
+      .then(res => res.json())
+      .then(data => setStory(data));
+  }, []);
+
   return (
     <div className="storyForm">
       <div className="header">
@@ -9,13 +16,18 @@ function StoryForm() {
         <button>모두 보기</button>
       </div>
       <div className="storyList">
-        <Storyitem />
-        <Storyitem />
-        <Storyitem />
-        <Storyitem />
-        <Storyitem />
-        <Storyitem />
-        <Storyitem />
+        {story &&
+          story.map(list => {
+            return (
+              <div className="storyItem" key={list.id}>
+                <img src="/images/kyungsuh/basic-profile-img.png" alt="" />
+                <div className="right">
+                  <span>{list.name}</span>
+                  <span>{list.time}</span>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
